efbdc29a850bf39f2ca0eaf938a35477557246b9
Ashwanth A R <ashwanth1109@gmail.com>
2018-12-04 02:36:32 +0530

    Set up basic slideshow component. Refactored CRA to essentials

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Slideshow from "./Slideshow";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Slideshow />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Slideshow.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+const s = {
+    container: "fullW height20 flex center"
+};
+
+class Slideshow extends Component {
+    render() {
+        return (
+            <div className={s.container}>
+                <div>Slideshow</div>
+            </div>
+        );
+    }
+}
+
+export default Slideshow;
