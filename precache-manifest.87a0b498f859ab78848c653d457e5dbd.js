6a90eaf73023807a1c28db1dee9a29a6447f02e0
Ashwanth A R <ashwanth1109@gmail.com>
2018-12-04 02:30:28 +0530

    Updates

---
+++ precache-manifest.87a0b498f859ab78848c653d457e5dbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactSlideshow/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7351b09fe15752b022cc",
+    "url": "/ReactSlideshow/static/js/runtime~main.7351b09f.js"
+  },
+  {
+    "revision": "cbd020e173254acf20de",
+    "url": "/ReactSlideshow/static/js/main.cbd020e1.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/ReactSlideshow/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "cbd020e173254acf20de",
+    "url": "/ReactSlideshow/static/css/main.28e59901.chunk.css"
+  },
+  {
+    "revision": "f044e4ee349a5adabd38acfb15090a7f",
+    "url": "/ReactSlideshow/index.html"
+  }
+];
