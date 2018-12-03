25dae46ec17f82e50cff7b1ab8493c4d58f8cf17
Ashwanth A R <ashwanth1109@gmail.com>
2018-12-04 02:14:56 +0530

    Refactoring app to set up with CRA

--- index.js
+++
@@ -1,1 +0,0 @@
-ReactDOM.render(<App />, document.querySelector("main"));
--- slideshow.js
+++
@@ -1,9 +0,0 @@
-class Slideshow extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello Slideshow</h1>
-            </div>
-        );
-    }
-}
